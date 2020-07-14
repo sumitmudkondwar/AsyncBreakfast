e2207d0b222ab227642ea741739903d1b7529714
Sumit Mudkondwar <sumit.mudkondwar@thomsonreuters.com>
2020-07-14 08:38:05 +0530

    Add project files.

---
+++ AsyncBreakfast/Breakfast2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncBreakfast
+{
+    public class Breakfast2
+    {
+        public static async void PrepareBreakfast()
+        {
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffee is ready");
+
+            Egg eggs = await FryEggsAsync(2);
+            Console.WriteLine("eggs are ready");
+
+            Bacon bacon = await FryBaconAsync(3);
+            Console.WriteLine("bacon is ready");
+
+            Toast toast = await ToastBreadAsync(2);
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+            Console.WriteLine("Breakfast is ready!");
+        }
+
+        private static Juice PourOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        private static void ApplyJam(Toast toast) =>
+            Console.WriteLine("Putting jam on the toast");
+
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on the toast");
+
+        private static async Task<Toast> ToastBreadAsync(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+            Console.WriteLine("Start toasting...");
+            await Task.Delay(3000);
+            Console.WriteLine("Remove toast from toaster");
+
+            return new Toast();
+        }
+
+        private static async Task<Bacon> FryBaconAsync(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+            Console.WriteLine("cooking first side of bacon...");
+            await Task.Delay(3000);
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+            Console.WriteLine("cooking the second side of bacon...");
+            await Task.Delay(3000);
+            Console.WriteLine("Put bacon on plate");
+
+            return new Bacon();
+        }
+
+        private static async Task<Egg> FryEggsAsync(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            await Task.Delay(3000);
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            await Task.Delay(3000);
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
+        }
+    }
+}
---
+++ AsyncBreakfast/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncBreakfast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Breakfast0.PrepareBreakfast();
+            Console.ReadLine();
+        }
+        
+    }
+}
